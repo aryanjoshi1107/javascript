add47b48c12342df20679b24831817bcd0d7c34a
Aryan Joshi <81485452+aryanjoshi1107@users.noreply.github.com>
2023-09-18 15:20:54 +0000

    basic variable learned

---
+++ basic/variable.js
@@ -0,0 +1,20 @@
+const accountId=3322323
+
+let accountEmail="joshiaryan1107@gmail.com"
+var accountName="aryan"
+accountCity="jaipur"//not preffered to use this sort of declaration
+
+// accountId=2323   this will throw error  because we can not modify the value of const 
+
+accountEmail="joshiaryan7820@gmail.com"
+accountName="aryan"
+accountCity="rishikesh"
+
+// preffered not to use var keyword beacuse of it scope 
+
+
+console.table([accountEmail,accountEmail,accoutnna,accountCity])
+
+
+
+
