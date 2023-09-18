875720c6938a7aa81f5ba86a10fd6b03c764f216
Aryan Joshi <81485452+aryanjoshi1107@users.noreply.github.com>
2023-09-18 15:45:44 +0000

    data type basic learning

---
+++ basic/data_types.js
@@ -0,0 +1,22 @@
+"use strict"
+
+//alert(2+3)//we are using nodejs,not browser
+
+console.log(4+4);
+
+//these are primitive data types
+
+// number=> 2 to power 53
+//for higher values we use bigint
+
+// boolean=> true/ false
+// null => standalone value
+// undefined 
+// symbol => uniqueness
+
+
+//Non-primitive data type
+//object 
+
+
+//note-- undefinded is a has undefiened type but null is an object
